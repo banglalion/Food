738007015fc88a5fd3d5b09a2d0a6cdf5e780a55
banglalion <83490432+banglalion@users.noreply.github.com>
2021-05-02 23:25:38 +0600

    Add files via upload

---
+++ food/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#banner-box').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:'<i class="fas fa-chevron-left aft"></i>',
+        nextArrow:'<i class="fas fa-chevron-right art"></i>', 
+        dotsClass:'banner-dots'
+      });
+      
+});
